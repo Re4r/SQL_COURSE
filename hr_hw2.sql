55bce430b88cbc9bc6fcfb3a6592b54813001be2
KirillMar <re4r@bk.ru>
2023-05-22 20:32:31 +0300

    select

--- hr_hw2.sql
+++ hr_hw2.sql
@@ -7,3 +7,31 @@ last_name,
 salary
 FROM employees;
 
+SELECT 
+employee_id,
+email,
+hire_date,
+hire_date-7 AS one_week_before_hire
+FROM employees;
+
+SELECT 
+first_name||' '||'('||job_id||')' AS our_employees 
+FROM employees;
+
+SELECT
+DISTINCT first_name 
+FROM employees;
+
+SELECT
+job_title,
+'min = '||min_salary||' '||'max = '||max_salary AS info,
+max_salary AS max,
+(max_salary*2)-200 AS new_salary
+FROM jobs;
+
+
+
+
+
+
+
