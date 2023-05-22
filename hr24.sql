11eee96d2c1f75c53947f00292c15f8b86e4bfde
KirillMar <re4r@bk.ru>
2023-05-22 19:21:51 +0300

    select

--- hr24.sql
+++ hr24.sql
@@ -2,3 +2,15 @@ SELECT * FROM employees;
 
 SELECT employee_id*2 + salary + department_id*10 
 FROM employees;
+
+SELECT first_name, salary, salary*2
+FROM employees;
+
+SELECT * FROM job_history;
+
+SELECT employee_id,
+start_date, end_date, 
+start_date+7, 
+end_date - start_date 
+FROM job_history;
+
