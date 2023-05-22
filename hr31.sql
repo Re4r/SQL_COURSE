ba5f518270d6494243b00c2f12fe4dbd6ce0b4d0
KirillMar <re4r@bk.ru>
2023-05-22 21:21:55 +0300

    select

---
+++ hr31.sql
@@ -0,0 +1,34 @@
+SELECT * FROM employees;
+
+SELECT
+first_name,
+last_name,
+salary
+FROM employees
+WHERE salary = 17000;
+
+SELECT
+first_name,
+last_name,
+salary
+FROM employees
+WHERE last_name = 'King';
+
+SELECT
+first_name,
+last_name
+FROM employees
+WHERE hire_date = '21.09.05';
+
+SELECT 
+DEPARTMENT_ID ,
+DEPARTMENT_NAME ,
+MANAGER_ID ,
+LOCATION_ID  
+FROM departments 
+WHERE location_id = 1700;
+
+SELECT * 
+FROM job_history 
+WHERE job_id = 'ST_CLERK';
+
