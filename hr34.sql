eea2a2a116c7378031d335ddb8592d53de3b99d6
KirillMar <re4r@bk.ru>
2023-05-23 18:56:09 +0300

    select

--- hr34.sql
+++ hr34.sql
@@ -13,3 +13,23 @@ FROM employees
 WHERE first_name 
 LIKE 'A%' AND salary > 5000;
 
+SELECT last_name 
+FROM employees 
+WHERE last_name LIKE 'D%' OR last_name LIKE '%d';
+
+SELECT * 
+FROM employees 
+WHERE NOT commission_pct IS NULL;
+
+SELECT * 
+FROM employees 
+WHERE salary NOT BETWEEN 5000 AND 10000;
+
+SELECT * 
+FROM employees 
+WHERE manager_id NOT IN (100);
+
+SELECT * 
+FROM employees 
+WHERE phone_number NOT LIKE '%6';
+
