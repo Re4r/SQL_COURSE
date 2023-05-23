7ddb15d740dcadc545595891ef20216fe0f8cc5f
KirillMar <re4r@bk.ru>
2023-05-23 18:26:34 +0300

    select

--- hr34.sql
+++ hr34.sql
@@ -1,1 +1,15 @@
 SELECT * FROM employees WHERE first_name LIKE 'S%';
+
+SELECT * FROM employees WHERE first_name LIKE 'D_n';
+
+SELECT * FROM employees WHERE first_name LIKE 'A%d%';
+
+SELECT last_name FROM employees WHERE last_name LIKE '__n%';
+
+SELECT * FROM employees WHERE job_id LIKE 'ST\_%' ESCAPE '\';
+
+SELECT * 
+FROM employees 
+WHERE first_name 
+LIKE 'A%' AND salary > 5000;
+
