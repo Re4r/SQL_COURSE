d4ab5db719ebd7823aaa146f6fb3861d0f1d964b
KirillMar <re4r@bk.ru>
2023-05-23 19:58:43 +0300

    select

--- hr39.sql
+++ hr39.sql
@@ -11,7 +11,7 @@ FIRST_NAME,
 LAST_NAME,
 SALARY
 FROM employees 
-WHERE job_id IN ('IT_PROG') ORDER BY salary;
+WHERE job_id IN ('IT_PROG') ORDER BY salary DESC;
 
 SELECT 
 employee_id AS id, 
@@ -20,6 +20,23 @@ last_name,
 job_id,
 hire_date 
 FROM employees 
-WHERE job_id IN ('ST_CLERK') ORDER BY hire_date;
+WHERE job_id IN ('ST_CLERK') ORDER BY hire_date DESC;
 
+SELECT 
+commission_pct 
+FROM employees 
+ORDER BY commission_pct 
+DESC NULLS LAST;
+
+SELECT 
+phone_number,
+first_name
+FROM employees ORDER BY 2;
+
+SELECT 
+job_id, 
+first_name, 
+salary 
+FROM employees
+ORDER BY 1 DESC, 2, 3 DESC;
 
