4cc88f7617a7e6fffe898b1c116d8b19880ae710
KirillMar <re4r@bk.ru>
2023-05-23 17:45:51 +0300

    select

--- hr31.sql
+++ hr31.sql
@@ -45,7 +45,32 @@ SELECT first_name, last_name, salary
 FROM employees 
 WHERE salary BETWEEN 4000 AND 10000;
 
+SELECT * 
+FROM job_history 
+WHERE start_date BETWEEN '01.01.04' AND '01.01.05';
 
+SELECT DISTINCT 
+department_name, location_id 
+FROM departments 
+WHERE location_id IN (1700, 2500);
+
+SELECT * FROM job_history;
+
+DESCRIBE job_history;
+
+SELECT EMPLOYEE_ID ,
+START_DATE ,
+END_DATE ,
+JOB_ID ,
+DEPARTMENT_ID  
+FROM job_history 
+WHERE job_id IN ('IT_PROG');
+
+SELECT EMPLOYEE_ID,
+FIRST_NAME,
+LAST_NAME,
+COMMISSION_PCT
+FROM employees WHERE commission_pct IS NULL;
 
 
 
