5e5fd0274cd4555c7c46c1b242ad196778ae245f
KirillMar <re4r@bk.ru>
2023-05-25 18:03:42 +0300

    functions

--- hr42.sql
+++ hr42.sql
@@ -8,3 +8,39 @@ FROM employees;
 SELECT * 
 FROM employees 
 WHERE LOWER(first_name) IN ('david');
+
+SELECT * 
+FROM employees 
+WHERE LOWER (first_name) LIKE 'd%v%';
+
+SELECT 
+last_name, 
+UPPER(last_name) 
+FROM employees;
+
+SELECT
+email,
+job_id,
+LOWER(email),
+LOWER(job_id)
+FROM employees
+ORDER BY job_id, email DESC;
+
+SELECT INITCAP('dDDDDD kKKKKK lLLLLLL') FROM dual;
+
+SELECT CONCAT('today is ', SYSDATE) FROM dual;
+
+SELECT 
+CONCAT('AAA ', CONCAT('BBB ', CONCAT('CCC ', 'DDD '))) AS example
+FROM dual;
+
+
+
+
+
+
+
+
+
+
+
