23e2738fec1c596bab1917b2ad90bf9f1bc2241f
KirillMar <re4r@bk.ru>
2023-05-26 17:32:09 +0300

    functions

---
+++ hr4hw.sql
@@ -0,0 +1,23 @@
+SELECT 
+first_name,
+LENGTH(first_name) AS lof
+FROM employees
+WHERE LENGTH(first_name) > 10;
+
+SELECT 
+salary,
+employee_id
+FROM employees
+WHERE MOD(salary, 1000) = 0;
+
+DESCRIBE employees;
+
+SELECT 
+phone_number,
+LENGTH(phone_number) AS l_t_n,
+SUBSTR(phone_number, 1, 3) AS s_s
+FROM employees
+WHERE LENGTH(phone_number) = 12;
+
+
+
