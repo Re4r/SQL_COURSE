97e714e94942d1b75b9eb9b82d87c7100a5afc59
KirillMar <re4r@bk.ru>
2023-05-29 19:57:40 +0300

    general_functions

--- hr57.sql
+++ hr57.sql
@@ -33,6 +33,25 @@ SELECT
 NULLIF(employee_id, manager_id)
 FROM employees;
 
+SELECT 
+country_id,
+country_name,
+NULLIF(country_id, UPPER(SUBSTR(country_name, 1, 2))) AS id,
+INITCAP(NVL2(NULLIF(country_id, UPPER(SUBSTR(country_name, 1, 2))), 'match', 'not match')) AS match
+FROM countries
+ORDER BY NULLIF(country_id, UPPER(SUBSTR(country_name, 1, 2))) DESC;
+
+SELECT
+first_name,
+last_name,
+email,
+UPPER(SUBSTR(first_name, 1, 1) || last_name) AS e_name,
+NULLIF((UPPER(SUBSTR(first_name, 1, 1) || last_name)),
+(email)) AS equal,
+NVL2(NULLIF((UPPER(SUBSTR(first_name, 1, 1) || last_name)),
+(email)), 'ok', 'not') AS test_case
+FROM employees
+ORDER BY equal DESC;
 
 
 
