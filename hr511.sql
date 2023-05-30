be3e0ca171c874c7effbabd8007db68b6b95494b
KirillMar <re4r@bk.ru>
2023-05-30 11:29:45 +0300

    conditional_functions

--- hr511.sql
+++ hr511.sql
@@ -3,3 +3,40 @@ commission_pct AS pct,
 DECODE(commission_pct, null, 'no bonus', 0.3, 'light', 0.4, 'medium', 'nothing') AS code
 FROM employees
 ORDER BY pct;
+
+SELECT 
+CASE salary
+WHEN 1000 THEN 'one'
+WHEN 2000 THEN 'two'
+WHEN 3000 THEN first_name
+WHEN 10000 THEN last_name
+ELSE email
+END AS result
+FROM employees
+ORDER BY result;
+
+SELECT 
+first_name,
+CASE LENGTH(first_name)
+WHEN 4 THEN '4'
+WHEN 5 THEN '5'
+WHEN 6 THEN '6'
+ELSE 'too long'
+END AS name_length
+FROM employees
+ORDER BY name_length;
+
+SELECT
+salary,
+CASE
+WHEN salary > 5000 THEN 'high'
+WHEN salary > 3000 AND salary < 5000 THEN 'medium'
+WHEN salary < 3000 THEN 'low'
+ELSE 'normal'
+END AS good
+FROM employees
+ORDER BY good;
+
+
+
+
