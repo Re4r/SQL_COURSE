2ab45251ed20266121c144f93bdf44c48d353c71
KirillMar <re4r@bk.ru>
2023-05-30 13:02:41 +0300

    homework5

--- hr5hw.sql
+++ hr5hw.sql
@@ -14,6 +14,25 @@ SUBSTR(department_name, 1, INSTR(department_name, ' ')) AS result
 FROM departments
 WHERE INSTR(department_name, ' ') != 0;
 
+SELECT 
+first_name,
+SUBSTR(first_name, 2, LENGTH(first_name) - 2) AS result
+FROM employees;
+
+SELECT 
+job_id
+FROM employees
+WHERE (LENGTH(job_id) - INSTR(job_id, '_')) >= 3 
+AND
+SUBSTR(job_id, INSTR(job_id, '_') + 1) != 'CLERK';
+
+SELECT 
+first_name,
+hire_date,
+TRUNC(hire_date, 'MM') AS result
+FROM employees
+WHERE hire_date = TRUNC(hire_date, 'MM');
+
 
 
 
