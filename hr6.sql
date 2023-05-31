98834c672c23d452b71d8b03987cd54917dd600f
KirillMar <re4r@bk.ru>
2023-05-31 15:11:13 +0300

    group_functions

--- hr6.sql
+++ hr6.sql
@@ -33,6 +33,46 @@ SELECT
 SUM(TRUNC(SYSDATE - hire_date))
 FROM employees;
 
+SELECT 
+ROUND(AVG(LENGTH(first_name || last_name))) AS result
+FROM employees;
+
+SELECT 
+AVG(DISTINCT salary)
+FROM employees
+WHERE LENGTH(last_name) < 5;
+
+SELECT 
+COUNT(salary),
+SUM(salary),
+AVG(salary),
+MIN(salary),
+MAX(salary)
+FROM employees;
+
+SELECT 
+ROUND(AVG(MONTHS_BETWEEN(SYSDATE,hire_date)) / 12) AS result
+FROM employees;
+
+SELECT 
+MIN(salary),
+MAX(salary)
+FROM employees
+WHERE department_id = 50;
+
+SELECT 
+MIN(hire_date),
+MAX(hire_date)
+FROM employees;
+
+SELECT 
+MIN(last_name),
+MAX(last_name)
+FROM employees;
+
+
+
+
 
 
 
