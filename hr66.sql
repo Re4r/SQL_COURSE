1e8a773e5400efdb15dd1700bd23165150df0161
KirillMar <re4r@bk.ru>
2023-06-01 19:23:03 +0300

    group_functions

--- hr66.sql
+++ hr66.sql
@@ -6,4 +6,75 @@ MAX(salary),
 ROUND(AVG(salary))
 FROM employees 
 GROUP BY department_id
-ORDER BY 1
+ORDER BY 1;
+
+SELECT 
+job_id,
+COUNT(*),
+ROUND(AVG(salary)) AS average
+FROM employees
+WHERE salary > 5000
+GROUP BY job_id
+ORDER BY average;
+
+SELECT
+TO_CHAR(hire_date, 'Month') AS w_month,
+COUNT(*) AS quantity,
+ROUND(AVG(salary)) AS aver
+FROM employees
+GROUP BY TO_CHAR(hire_date, 'Month')
+ORDER BY quantity;
+
+SELECT
+department_id,
+MAX(hire_date),
+MIN(first_name),
+COUNT(*),
+SUM(salary),
+ROUND(AVG(employee_id))
+FROM employees
+GROUP BY department_id;
+
+SELECT
+department_id,
+job_id,
+COUNT(*)
+FROM employees
+GROUP BY department_id, job_id
+ORDER BY 1, 2;
+
+SELECT 
+job_id,
+TO_CHAR(hire_date, 'YYYY') AS h_year,
+COUNT(*),
+ROUND(AVG(salary)) AS a_salary,
+ROUND(AVG(NVL(commission_pct, 0)), '9.9') a_cpct
+FROM employees
+GROUP BY job_id, TO_CHAR(hire_date, 'YYYY')
+ORDER BY job_id ,h_year; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
