73e4e5767c573172de06a814b476509b17c9d17a
KirillMar <re4r@bk.ru>
2023-06-01 20:22:21 +0300

    homework_6

--- hr6hw.sql
+++ hr6hw.sql
@@ -10,3 +10,10 @@ FROM employees
 GROUP BY department_id
 ORDER BY m_quantity DESC;
 
+SELECT
+SUBSTR(first_name,1,1) AS flon,
+COUNT(*) AS m_count
+FROM employees
+GROUP BY SUBSTR(first_name,1,1)
+HAVING COUNT(*) > 1
+ORDER BY m_count DESC, flon;
