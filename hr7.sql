396a04ab88fc05ef1632d8e2f2685c355a0cc816
KirillMar <re4r@bk.ru>
2023-06-02 12:52:29 +0300

    join

--- hr7.sql
+++ hr7.sql
@@ -1,6 +1,23 @@
 SELECT * FROM regions;
 SELECT * FROM countries;
 
-SELECT * 
-FROM regions
-NATURAL JOIN countries;
+SELECT 
+c.country_name,
+c.country_id,
+region_id,
+r.region_name
+FROM regions r
+NATURAL JOIN countries c;
+
+SELECT * FROM employees;
+SELECT * FROM departments;
+
+SELECT 
+departments.department_name,
+department_id,
+manager_id,
+employees.first_name,
+employees.last_name,
+employees.salary
+FROM employees NATURAL JOIN departments;
+
