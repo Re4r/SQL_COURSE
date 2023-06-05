753677db09215410f1f2e9f15afe69f1c5106769
KirillMar <re4r@bk.ru>
2023-06-05 20:51:31 +0300

    homework7

--- hr7hw.sql
+++ hr7hw.sql
@@ -14,4 +14,22 @@ JOIN departments dep ON loc.location_id = dep.location_id
 JOIN employees emp ON emp.department_id = dep.department_id
 GROUP BY reg.region_name;
 
+SELECT 
+emp.first_name,
+emp.last_name,
+dep.department_name,
+emp.job_id,
+j.job_title,
+loc.street_address,
+loc.city,
+loc.state_province,
+c.country_name,
+r.region_name
+FROM employees emp
+JOIN departments dep ON emp.department_id = dep.department_id
+JOIN jobs j ON j.job_id = emp.job_id
+JOIN locations loc ON loc.location_id = dep.location_id
+JOIN countries c ON c.country_id = loc.country_id
+JOIN regions r ON r.region_id = c.region_id;
+
 
