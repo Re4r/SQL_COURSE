737560a23b60bd3b460b5b082667524226401d4d
KirillMar <re4r@bk.ru>
2023-06-05 19:22:54 +0300

    outer_join

--- hr78.sql
+++ hr78.sql
@@ -2,7 +2,40 @@ SELECT
 emp.first_name,
 emp.last_name,
 emp.salary,
-dep.department_name
+dep.department_name,
+dep.department_id
 FROM employees emp 
 LEFT OUTER JOIN departments dep 
 ON emp.department_id = dep.department_id; 
+
+SELECT
+emp.first_name,
+emp.last_name,
+emp.salary,
+dep.department_name
+FROM departments dep 
+LEFT OUTER JOIN employees emp 
+ON emp.department_id = dep.department_id; 
+
+SELECT
+emp.first_name,
+emp.last_name,
+emp.salary,
+dep.department_name,
+dep.department_id
+FROM employees emp 
+RIGHT OUTER JOIN departments dep 
+ON emp.department_id = dep.department_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
