2195c81a633225f511752844aa6ff9cd86abee48
KirillMar <re4r@bk.ru>
2023-06-07 19:23:15 +0300

    subquery

--- hr8.sql
+++ hr8.sql
@@ -18,7 +18,41 @@ FROM employees
 WHERE employee_id IN (SELECT employees.manager_id FROM employees)
 ORDER BY job_id;
 
+SELECT
+department_name,
+MIN(salary),
+MAX(salary)
+FROM 
+(SELECT salary, department_name 
+FROM employees e 
+JOIN departments d 
+ON e.department_id = d.department_id)
+GROUP BY department_name;
 
+SELECT 
+first_name,
+last_name,
+salary
+FROM employees
+WHERE salary < (SELECT MAX(salary) / 5 FROM employees);
+
+SELECT 
+e.first_name,
+e.last_name,
+e.salary
+FROM employees e
+WHERE e.salary > (SELECT AVG(e2.salary) FROM employees e2);
+
+SELECT AVG(e2.salary) FROM employees e2;
+
+SELECT 
+e1.first_name,
+e1.last_name,
+e1.salary
+FROM employees e1
+WHERE e1.salary >= (SELECT e2.salary FROM employees e2 WHERE e2.employee_id = 180);
+
+SELECT e2.salary FROM employees e2 WHERE e2.employee_id = 180;
 
 
 
