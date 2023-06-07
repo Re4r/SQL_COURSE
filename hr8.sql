c92d6a35472d73c52acd3d3f3a597223c7b33e62
KirillMar <re4r@bk.ru>
2023-06-07 18:37:28 +0300

    subquery

--- hr8.sql
+++ hr8.sql
@@ -6,6 +6,23 @@ FROM employees
 WHERE salary > (SELECT AVG(salary) FROM employees);
 
 SELECT 
-(SELECT MIN(jobs.min_salary) FROM jobs) ms,
-(SELECT MAX(employees.first_name) FROM employees) mnl
+(SELECT MIN(jobs.min_salary) FROM jobs) min_s,
+(SELECT MAX(LENGTH(employees.first_name)) FROM employees) max_nl
 FROM dual;
+
+SELECT 
+first_name,
+last_name,
+job_id
+FROM employees
+WHERE employee_id IN (SELECT employees.manager_id FROM employees)
+ORDER BY job_id;
+
+
+
+
+
+
+
+
+
