ae0d0d2da360e86b4d04fb447b10b1b546d993c8
KirillMar <re4r@bk.ru>
2023-06-08 12:05:35 +0300

    homework8

--- hr8hw.sql
+++ hr8hw.sql
@@ -7,3 +7,17 @@ SELECT *
 FROM employees e1
 WHERE e1.salary > (SELECT AVG(e2.salary) FROM employees e2);
 
+SELECT 
+l.city,
+SUM(e.salary)
+FROM locations l 
+JOIN departments d ON l.location_id = d.location_id
+JOIN employees e ON e.department_id = d.department_id
+GROUP BY l.city;
+
+
+
+
+
+
+
