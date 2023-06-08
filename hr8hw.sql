6eb364ec4601fb2f19b95e2aeb13d47fc6fd3f52
KirillMar <re4r@bk.ru>
2023-06-08 19:31:44 +0300

    homework8

--- hr8hw.sql
+++ hr8hw.sql
@@ -13,7 +13,37 @@ SUM(e.salary)
 FROM locations l 
 JOIN departments d ON l.location_id = d.location_id
 JOIN employees e ON e.department_id = d.department_id
-GROUP BY l.city;
+GROUP BY l.city
+HAVING SUM(e.salary) = 
+(SELECT 
+MIN(SUM(e1.salary)) 
+FROM employees e1
+JOIN departments d1 ON e1.department_id = d1.department_id
+JOIN locations l1 ON d1.location_id = l1.location_id
+GROUP BY l1.city);
+
+SELECT 
+d.department_name
+FROM employees e 
+RIGHT OUTER JOIN departments d ON e.department_id = d.department_id
+WHERE e.employee_id IS NULL;
+
+SELECT * 
+FROM employees e
+WHERE e.job_id IN 
+(SELECT j.job_id FROM jobs j WHERE j.job_title NOT LIKE '%Manager');
+
+SELECT * FROM jobs;
+SELECT * FROM employees;
+
+SELECT
+*
+FROM jobs j JOIN employees e ON j.job_id = e.job_id
+WHERE j.job_title LIKE '%Manager'
+
+
+
+
 
 
 
