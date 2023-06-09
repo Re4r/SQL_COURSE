773b095a528ab5c2d128b9e4dc32cfa00f126988
KirillMar <re4r@bk.ru>
2023-06-09 13:45:48 +0300

    set_operators

--- hr9.sql
+++ hr9.sql
@@ -9,3 +9,29 @@ SELECT salary FROM employees WHERE salary > 10000
 UNION
 SELECT jobs.max_salary FROM jobs WHERE jobs.max_salary < 15000;
 
+SELECT job_id FROM employees
+INTERSECT
+SELECT jobs.job_id FROM jobs; 
+
+SELECT salary FROM employees WHERE salary > 10000
+MINUS
+SELECT jobs.max_salary FROM jobs WHERE jobs.max_salary < 15000;
+
+SELECT first_name, salary FROM employees WHERE first_name LIKE '%a%'
+INTERSECT
+SELECT first_name, salary FROM employees WHERE first_name LIKE '%e%'
+MINUS
+SELECT first_name, salary FROM employees WHERE first_name LIKE '%l%';
+
+
+
+
+
+
+
+
+
+
+
+
+
