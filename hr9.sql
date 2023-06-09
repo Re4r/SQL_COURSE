26e973e8ce777cb151c4889b6e759b86a43f0744
KirillMar <re4r@bk.ru>
2023-06-09 13:27:35 +0300

    set_operators

--- hr9.sql
+++ hr9.sql
@@ -4,3 +4,8 @@ SELECT email
 UNION ALL
 SELECT regions.region_name 
     FROM regions;
+    
+SELECT salary FROM employees WHERE salary > 10000
+UNION
+SELECT jobs.max_salary FROM jobs WHERE jobs.max_salary < 15000;
+
